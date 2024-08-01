aaf0eafc37f33b3099dae84a9c338bd9e2474755
Thati05 <m.seithati01@gmail.com>
2024-08-01 20:48:16 +0200

    commit

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing (models.Model):
+    image_url = models.URLField( max_length=500, null=False, blank=False)
+    title = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
