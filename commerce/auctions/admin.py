32dfea8a6598bd9807d9cbf4325005478257e4de
Thati05 <m.seithati01@gmail.com>
2024-08-02 12:39:18 +0200

    commit

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(CreateListing)
+admin.site.register(AuctionList)
+admin.site.register(User)
+
