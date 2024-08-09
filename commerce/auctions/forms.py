ab2b9750c779b316f5eac9bdca5c5a8e1aac1dc6
Thati05 <m.seithati01@gmail.com>
2024-08-09 02:03:48 +0200

    commit

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -5,5 +5,11 @@ class AuctionListingForm(forms.ModelForm):
     class Meta:
         model = CreateListing
         fields = '__all__'
-
+        widgets = {
+            'image_url': forms.URLInput(attrs={'size': 40}),
+            'title': forms.TextInput(attrs={'size': 30}),
+            'description': forms.Textarea(attrs={'cols': 20, 'rows': 4}),
+            'price': forms.NumberInput(attrs={'size': 10}),
+            'category': forms.Select(attrs={'size': 1}),
+        }
 
