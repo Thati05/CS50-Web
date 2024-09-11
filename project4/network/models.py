7176b291dcc0da8bb768157bc1b2d13fc3b07619
Thati05 <m.seithati01@gmail.com>
2024-09-11 23:32:20 +0200

    commit

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+#Post Model
+class Post(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta: 
+        ordering = ('-created_at',) # N/B the comma bc this is a tuple 
+    
+    def __str__(self):
+        return f"{self.creator.username}: {self.content[0:10]}..."
+    
+
+    
