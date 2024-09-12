9d4e970f02b20cef17ec359e35b988e1ef008226
Thati05 <m.seithati01@gmail.com>
2024-09-12 19:00:46 +0200

    commit

---
+++ project4/frontend/networkapi/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+
+    <div className=' flex justify-between items-center p-10'>
+      <div className=' font-Nunito font-extrabold text-3xl' >
+        Network
+      </div>
+      <div className=' flex gap-7 items-center'>
+        <img width={20} src='https://cdn-icons-png.flaticon.com/512/2040/2040504.png'/>
+        <img width={20} src='https://cdn-icons-png.flaticon.com/512/3239/3239952.png'/>
+
+      </div>
+
+
+    </div>
+
+  
+  )
+}
+
+export default Header
---
+++ project4/frontend/networkapi/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Nunito: ['Nunito', 'sans-serif'],
+      Playwrite: ['Playwrite USA Modern', 'sans-serif'],
+      
+    },
+  },
+  plugins: [],
+}
