f9a091124d74ae65630fb072529e579c12e5b2a2
Thati05 <m.seithati01@gmail.com>
2024-09-13 00:42:23 +0200

    commit

--- project4/frontend/networkapi/src/components/Header.jsx
+++ project4/frontend/networkapi/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { userProfile } from '../constants'
 
 
 const Header = () => {
   return (
 
+    <section className=' '>
+
+      
     <div className=' flex justify-between items-center p-10'>
       <div className=' font-Nunito font-extrabold text-3xl' >
         Network
@@ -13,9 +17,30 @@ const Header = () => {
         <img width={20} src='https://cdn-icons-png.flaticon.com/512/3239/3239952.png'/>
 
       </div>
+     </div>
 
+     {/*Profile */}
+
+    <div className=' flex justify-center items-center mb-5  '>
+<div className=' flex items-center justify-between gap-24'>
+
+    { userProfile.map((profile) => (
+        <div className='justify-center rounded-xl w-[80px] h-[74px]  image-container img_border'>
+
+<img className='rounded-md  w-[60px] h-[53px] object-cover' key={profile.alt} src={profile.src} alt={profile.alt}/>
+
+
+      
+      </div>
+    ))}
+ 
+
+
+</div>
 
     </div>
+
+    </section>
 
   
   )
