6ffac84e7950561613647685f20e379ceda65865
Thati05 <m.seithati01@gmail.com>
2024-09-16 19:47:27 +0200

    commit

--- project4/frontend/networkapi/src/components/SidebarRight.jsx
+++ project4/frontend/networkapi/src/components/SidebarRight.jsx
@@ -2,36 +2,80 @@ import React from 'react'
 
 const SidebarRight = () => {
   return (
-    <section className=' font-Nunito font-semibold text-lg  mr-5 CIK-rightside' >
-    <div className=' flex flex-col gap-10'>
+    <section className='  font-Nunito font-semibold text-lg  mr-5 CIK-rightside' >
+    <div className=' items-center flex flex-col gap-10'>
  
- <div className=' flex items-center side-item bg-[#F5F5F5]'>
 
-    <img  width={30} height={30}  src='https://cdn-icons-png.flaticon.com/512/1946/1946436.png'/>
- 
-   
-   <p className='ml-5'>Home</p>
+
+ <div className=' flex items-center pb-5 pr-10 border-b-[#7575754c] border-b-[1px]  '>
+
+ <div className="bg-gradient">
+  <div className="justify-center rounded-[11px] w-[59px] h-[59px] image-container img_border">
+    <img
+    src='https://img.freepik.com/premium-photo/portrait-beautiful-young-asian-woman-neon-light_1308175-55057.jpg'
+    className="rounded-md w-[50px] h-[50px] object-cover"
+     />
+     </div>
+     </div>
+  <div className=' ml-5'>
+    <p>Username</p>
+    <p>User@Email</p>
+  </div>
+
  </div>
 
- <div className=' flex items-center side-item  '>
-  <img  width={30} height={30} className=' object-contain'  src='https://cdn-icons-png.flaticon.com/512/10146/10146694.png'/>
-  <p className='ml-5' >Discover</p>
+ <h3 className=' font-bold'>Suggested for You</h3>
+
+ <div className=' flex items-center pr-10 '>
+
+ <div className="bg-gradient">
+  <div className="justify-center rounded-[11px] w-[59px] h-[59px] image-container img_border">
+    <img
+    src='https://img.freepik.com/free-photo/portrait-woman-…ts_23-2151626916.jpg'
+    className="rounded-md w-[50px] h-[50px] object-cover"
+     />
+     </div>
+     </div>
+  <div className=' ml-5'>
+    <p>Username</p>
+    <p>User@Email</p>
+  </div>
+
  </div>
 
- <div className=' flex items-center side-item  '>
-  <img  width={30} height={30} className=' object-contain'  src='https://cdn-icons-png.flaticon.com/512/542/542638.png'/>
-  <p className='ml-5' >Messages</p>
- </div>
+ <div className=' flex items-center pr-10'>
 
- <div className=' flex items-center side-item  '>
-  <img  width={30} height={30} className=' object-contain'  src='https://cdn-icons-png.flaticon.com/512/10146/10146694.png'/>
-  <p className='ml-5' >Profile</p>
- </div>
+ <div className="bg-gradient">
+  <div className="justify-center rounded-[11px] w-[59px] h-[59px] image-container img_border">
+    <img
+    src='https://img.freepik.com/premium-photo/medium-shot-woman-posing-with-orange-outfit_23-2150728981.jpg'
+    className="rounded-md w-[50px] h-[50px] object-cover"
+     />
+     </div>
+     </div>
+  <div className=' ml-5'>
+    <p>Username</p>
+    <p>User@Email</p>
+  </div>
 
- <div className=' flex items-center side-item  '>
-  <img  width={30} height={30} className=' object-contain'  src='https://cdn-icons-png.flaticon.com/512/10146/10146694.png'/>
-  <p className='ml-5' >Friends</p>
  </div>
+ {/*<div className=' flex items-center pr-10'>
+
+ <div className="bg-gradient">
+  <div className="justify-center rounded-[11px] w-[59px] h-[59px] image-container img_border">
+    <img
+    src='https://img.freepik.com/premium-photo/portrait-beautiful-young-asian-woman-neon-light_1308175-55057.jpg'
+    className="rounded-md w-[50px] h-[50px] object-cover"
+     />
+     </div>
+     </div>
+  <div className=' ml-5'>
+    <p>Username</p>
+    <p>User@Email</p>
+  </div>
+
+ </div>*/}
+
 
 
 </div>
