d98234c8c7d918010cd148294dc2377e918794f6
Thati05 <m.seithati01@gmail.com>
2024-09-18 17:16:41 +0200

    commit

--- project4/frontend/networkapi/src/components/Logout.jsx
+++ project4/frontend/networkapi/src/components/Logout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import axiosInstance from '../axios'
 
 const Logout = () => {
+  const navigate = useNavigate()
+
+useEffect(() => {
+
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('refresh_token');
+  localStorage.removeItem('username');
+  axiosInstance.defaults.headers["Authorization"] = null;
+
+  navigate('/login')
+
+})
+
+
   return (
-    <div>Logout</div>
+    <div>Logout out...</div>
   )
 }
 
